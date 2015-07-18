8bf696033a91398242a85d1dbe05d4cc58c1f601
vihaan <vihaan@192.168.1.104>
2015-07-18 13:37:54 +0530

    Second Changes for Master
    Change-Id: I0b40afb8b565fd20ebea364fc8acb8cb5034f438

---
+++ ManageGitRepository/src/TestString.java
@@ -0,0 +1,8 @@
+
+public class TestString {
+
+		public void displayProperString(){
+			System.out.println("TestString.displayProperString()");
+			System.out.println("TestString.displayProperString() RAJ RAJ RAJ");
+		}
+}
